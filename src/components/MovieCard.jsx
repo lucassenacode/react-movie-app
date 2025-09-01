c987c059c5827356a5fe5811f9b7260b0429f876
lucassenacode <lucassenacode@gmail.com>
2025-09-01 19:14:50 -0300

    feat: Adiciona navbar responsiva e melhorias de UI

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,6 +7,9 @@ const placeholderImage = "https://placehold.co/500x750/333/FFFFFF?text=Sem+Image
 const MovieCard = ({ movie }) => {
   return (
     <Link to={`/movie/${movie.id}`} className="movie-card">
+       <div className="rating-badge">
+          ⭐ {movie.vote_average.toFixed(1)}
+        </div>
       <img 
         src={movie.poster_path ? `${imageUrl}${movie.poster_path}` : placeholderImage} 
         alt={`Pôster do filme ${movie.title}`} 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+ 
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <Link to="/" className="navbar-logo">
         🎬 MovieFinder
       </Link>
-      <div className="navbar-links">
-        <Link to="/">Início</Link>
-        <Link to="/favorites">Favoritos</Link>
+
+      
+      <button className="hamburger-icon" onClick={() => setIsOpen(!isOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      
+      <div className={`navbar-links ${isOpen ? 'open' : ''}`}>
+        
+        <Link to="/" onClick={() => setIsOpen(false)}>Início</Link>
+        <Link to="/favorites" onClick={() => setIsOpen(false)}>Favoritos</Link>
       </div>
     </nav>
   );
